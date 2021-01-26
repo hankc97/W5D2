a3d8f64de28e12c8e94b05f1c16b397812bb0a5d
danny <dannyho77@gmail.com>
2021-01-26 16:27:07 -0500

    completed 03_select file

--- SQL_ZOO/lib/03_select_nobel.rb
+++ SQL_ZOO/lib/03_select_nobel.rb
@@ -36,18 +36,36 @@ end
 def literature_1962
   # Show who won the 1962 prize for Literature.
   execute(<<-SQL)
+    SELECT
+      winner
+      FROM
+        nobels
+        WHERE
+          subject = 'Literature' AND yr = 1962
   SQL
 end
 
 def einstein_prize
   # Show the year and subject that won 'Albert Einstein' his prize.
   execute(<<-SQL)
+    SELECT
+      yr, subject
+        FROM
+          nobels
+            WHERE
+              winner = 'Albert Einstein'
   SQL
 end
 
 def millennial_peace_prizes
   # Give the name of the 'Peace' winners since the year 2000, including 2000.
   execute(<<-SQL)
+    SELECT
+      winner
+        FROM
+          nobels
+            WHERE
+              yr >= 2000 AND subject LIKE 'Peace%'
   SQL
 end
 
@@ -55,6 +73,11 @@ def eighties_literature
   # Show all details (yr, subject, winner) of the Literature prize winners
   # for 1980 to 1989 inclusive.
   execute(<<-SQL)
+    SELECT *
+      FROM
+        nobels
+          WHERE
+            subject = 'Literature' AND yr BETWEEN 1980 AND 1989
   SQL
 end
 
@@ -62,11 +85,20 @@ def presidential_prizes
   # Show all details of the presidential winners: ('Theodore Roosevelt',
   # 'Woodrow Wilson', 'Jimmy Carter')
   execute(<<-SQL)
+    SELECT
+      *
+        FROM
+          nobels
+            WHERE
+              winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter')
   SQL
 end
 
 def nobel_johns
   # Show the winners with first name John
   execute(<<-SQL)
+    SELECT winner
+    FROM nobels
+    WHERE winner LIKE 'John%'
   SQL
 end
